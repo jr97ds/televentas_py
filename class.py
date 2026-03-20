6e27223530d4b7810cdbdd26d4d9ddfd901807f9
jorojas84 <jorojas84@unisalle.edu.co>
2026-03-20 10:22:51 -0500

    creacion clases de usuarios

--- class.py
+++ class.py
@@ -1,19 +1,46 @@
+from abc import ABC, abstractmethod
 
-# Clase del Cliente que accede al sistema para hacer compra/revisar pedidos
 
-class Cliente:
+# Clase abstracta que sirve de base para todas los usuarios involucrados
+class Persona(ABC):
+
     def __init__(self, nombre : str, apellido : str, 
-                 direccion : str, correo : str, telefono : str):
+                 correo : str):
         
         self.nombre = nombre
         self.apellido = apellido
-        self.direccion = direccion
         self.correo = correo
-        self.telefono = telefono
+        
+        
+
+class Cliente(Persona):
+
+    def __init__(self, nombre : str, apellido : str, 
+                 direccion : str, correo : str):
+        super().__init__(nombre, apellido, correo)
+        self.direccion = direccion
         self.suscripcion = None
         self.quejas = []
         self.ordenes_compra = []
 
+class Empleado(Persona, ABC):
 
+    def __init__(self, nombre : str, apellido : str, 
+                 correo : str, usuario : str, contraseña : str):
+        super().__init__(nombre, apellido, correo)
+        self.usuario = usuario
+        self.contraseña = contraseña
 
+class AgenteDeposito(Empleado):
 
+    def __init__(self, nombre : str, apellido : str, 
+                 correo : str, usuario : str, contraseña : str):
+        super().__init__(nombre, apellido, correo, usuario, 
+                         contraseña)
+        
+class GerenteRP(Empleado):
+
+    def __init__(self, nombre : str, apellido : str, 
+                 correo : str, usuario : str, contraseña : str):
+        super().__init__(nombre, apellido, correo, usuario, 
+                         contraseña)
