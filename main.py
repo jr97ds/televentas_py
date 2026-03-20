18c80cf0e08aba3dc365c7e8c473500eae3cd2aa
jorojas84 <jorojas84@unisalle.edu.co>
2026-03-20 16:11:42 -0500

    implementar carga y creacion de catalogo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from compras import OrdenCompra
 from personas import Cliente
+from producto import Producto , InventarioExcel , Catalogo , Suscripcion
 
 jose = Cliente("Jose", "Rojas", "Calle 123", "jose@gmail.com")
 
@@ -7,15 +8,26 @@ clientes = [jose]
 cliente_actual = None
 login = False
 
-# main.py
+#Creación de Productos a partir del inventario externo
+inventario = InventarioExcel("inventario_televentas.xlsx")
+
+# Funcion para inicializar el catalogo a partir de productos creados con inventairo
+def inicializar_catalogo(inventario : InventarioExcel) -> Catalogo:
+    catalogo = Catalogo()
+    for item in inventario.cargar_inventario_externo():
+        catalogo.agregar_producto(item)
+
+    return catalogo
+
+
+# menu para seleccionar portal de clientes o empleados
 def menu_usuario():
     print("\n" + "----- TeleVentas -----")
     print("1. Portal Clientes")
     print("2. Portal Empleados")
 
 
-# Funcion para crear nuevos clientes y añadirlos a la 
-# lista de clientes registrados
+# Funcion para crear nuevos clientes y añadirlos a la lista de clientes registrados
 def crear_cliente():
     nombre = input("Ingrese su nombre: ")
     apellido = input("Ingrese su apellido: ")
@@ -23,6 +35,11 @@ def crear_cliente():
     correo = input("Ingrese su correo electrónico: ")
     print("\n" + f"{nombre} , has sido registrado exitosamente")
     return Cliente(nombre, apellido, direccion, correo)
+
+
+# --------------------Inicialización del programa-------------------- #
+
+catalogo = inicializar_catalogo(inventario)
 
 while True:
     menu_usuario()
@@ -39,15 +56,15 @@ while True:
 
         opcion_cliente = input("\n" + "Seleccione una opción: ")
 
-        # Usuario Nuevo
+        # Registro Usuario Nuevo
         if opcion_cliente == "1":
             print("\n" + "Por favor regístrese para continuar")
             cliente_actual = crear_cliente()
             # añadir cliente a la lista de clientes registrados
             clientes.append(cliente_actual)
-            
+            login = True
 
-        # Usuario Registrado
+        # Login Usuario Registrado
         elif opcion_cliente == "2":
             print("\n" + "Por favor inicie sesión para continuar")
             while not login: 
@@ -81,10 +98,26 @@ while True:
 
             opcion_cliente_loggeado = input("\n" + "¿Que desea hacer?: ")
 
-            if opcion_cliente_loggeado == "0":
-                print("\n" + "Cerrando sesión...")
-                login = False
-                cliente_actual = None
+
+            # Mostrar Catalogo de Productos
+            if opcion_cliente_loggeado == "1":
+                print("\n" + "Catálogo de Productos:")
+                catalogo.mostrar_catalogo()
+
+                # Menu en vista de catalogo
+                print("\n" + "¿Que desea hacer?")
+                print("1. Realizar una orden de compra")
+                print("2. Volver al menú anterior")
+                opcion_orden = input("\n" + "Seleccione una opción: ")
+
+                if opcion_orden == "2":
+                    continue
+
+                # Cierre Sesion Cliente
+                if opcion_cliente_loggeado == "0":
+                    print("\n" + "Cerrando sesión...")
+                    login = False
+                    cliente_actual = None
 
         
 
