0882f092e7b22109ff103a96e60c602d71506211
jorojas84 <jorojas84@unisalle.edu.co>
2026-03-20 13:28:18 -0500

    añadir getters a cliente

--- personas.py
+++ personas.py
@@ -7,29 +7,50 @@ class Persona(ABC):
     def __init__(self, nombre : str, apellido : str, 
                  correo : str):
         
-        self.nombre = nombre
-        self.apellido = apellido
-        self.correo = correo
+        self._nombre = nombre
+        self._apellido = apellido
+        self._correo = correo
         
         
-
+# Clase para clientes
 class Cliente(Persona):
 
     def __init__(self, nombre : str, apellido : str, 
                  direccion : str, correo : str):
         super().__init__(nombre, apellido, correo)
-        self.direccion = direccion
-        self.suscripcion = None
-        self.quejas = []
-        self.ordenes_compra = []
+        self._direccion = direccion
+        self._suscripcion = None
+        self._quejas = []
+        self._ordenes_compra = []
 
+    @property
+    def correo(self):
+        return self._correo
+    
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @property
+    def apellido(self):
+        return self._apellido
+    
+    @property
+    def direccion(self):
+        return self._direccion
+    
+    @property
+    def nombre_completo(self):
+        return f"{self._nombre} {self._apellido}"
+
+# Clase abstracta para empleados
 class Empleado(Persona, ABC):
 
     def __init__(self, nombre : str, apellido : str, 
                  correo : str, usuario : str, contraseña : str):
         super().__init__(nombre, apellido, correo)
-        self.usuario = usuario
-        self.contraseña = contraseña
+        self._usuario = usuario
+        self._contraseña = contraseña
 
 class AgenteDeposito(Empleado):
 
