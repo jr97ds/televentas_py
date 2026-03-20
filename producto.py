437f173aee4d0974800dca3737c86aaa99a1100a
jorojas84 <jorojas84@unisalle.edu.co>
2026-03-20 15:33:11 -0500

    implementacion de interfaz inventario para traer datos de sistema externo

--- producto.py
+++ producto.py
@@ -1,29 +1,57 @@
 from personas import Cliente
-from abc import ABC
+from abc import ABC , abstractmethod
+from openpyxl import load_workbook
+
 class Producto: 
 
     def __init__(self, id_producto : str, nombre : str, 
         descripcion : str, precio : float, stock : int):
 
-        self.id_producto = id_producto
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.precio = precio
-        self.stock = stock
+        self._id_producto = id_producto
+        self._nombre = nombre
+        self._descripcion = descripcion
+        self._precio = precio
+        self._stock = stock
 
 
 class Catalogo:
     def __init__(self):
-        self.productos = []
+        self._productos = []
 
 
 class Suscripcion:
 
-    def __init__(self, cliente : Cliente, peridiocidad : str):
-        self.cliente = cliente
-        self.peridiocidad = peridiocidad
-        self.status = "Activa"
+    def __init__(self, cliente : Cliente):
+        self._cliente = cliente
+        self._status = "Activa"
 
 
+# Clase abstracta para inventarios - depende del sistema de inventario externo
 class Inventario(ABC):
 
+    @abstractmethod
+    def cargar_inventario_externo(self):
+        pass
+
+    #@abstractmethod
+    #def modificar_inventario_externo(self, producto : Producto, cantidad : int):
+        #pass    
+
+
+
+# Implementamos de forma supuesta para el ejercicio  un sistema de inventario externo basado en excel
+
+class InventarioExcel(Inventario):
+    def __init__(self, ruta_archivo : str):
+        self._ruta_archivo = ruta_archivo
+
+    def cargar_inventario_externo(self):
+        wb = load_workbook(self._ruta_archivo)
+        ws = wb.active
+        productos = []
+        for row in ws.iter_rows(min_row=2, values_only=True):
+            id_producto, nombre, descripcion, precio, stock = row
+            producto = Producto(id_producto, nombre, descripcion, precio, stock)
+            productos.append(producto)
+            wb.close()
+        return productos
